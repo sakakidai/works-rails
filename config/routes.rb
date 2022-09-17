000af066de2c1ce51dc04217adc48e558c66e463
sakakidai <sd.lybd1016@gmail.com>
2022-09-18 00:12:41 +0900

    Rubocopのオートコレクトを実施

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -5,5 +7,5 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   ## 開発環境用letter_opener
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
